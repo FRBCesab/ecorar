d2e6c79e7ddff7d27ea4ba8e7904abf3ed35d59a
nmouquet <nicolas.mouquet@cnrs.fr>
2019-01-24 17:08:56 +0100

    pcoa some change

--- scripts/Pcoa.R
+++ scripts/Pcoa.R
@@ -15,14 +15,14 @@ who.remote(remote=FALSE,who="NM")
 
 ##Birds 
 
-load(file=file.path(results_dir,"birds/50km/disTraits_birds.RData")) #TODO why the data are in 50km ? 
-load(file=file.path(results_dir,"birds//50km/birdstrait.RData")) #TODO why the data are in 50km ? 
+load(file=file.path(results_dir,"birds/50km/disTraits_birds.RData")) 
+load(file=file.path(results_dir,"birds/50km/birdstrait.RData"))
 load(file=file.path(results_dir,"birds/50km/FR_birds.RData"))
 
 ##Mammals 
 
-load(file=file.path(results_dir,"mammals/50km/disTraits_mammals.RData")) #TODO why the data are in 50km ? 
-load(file=file.path(results_dir,"mammals/50km/mammalstrait.RData")) #TODO why the data are in 50km ? 
+load(file=file.path(results_dir,"mammals/50km/disTraits_mammals.RData")) 
+load(file=file.path(results_dir,"mammals/50km/mammalstrait.RData"))
 load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
 
 #----
@@ -42,7 +42,7 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
 #LOAD PCOA ---- 
     
   load(file=file.path(results_dir,"birds/pco_birds.RData"))
-  load(file=file.path(results_dir,"mamals/pco_mamals.RData"))
+  load(file=file.path(results_dir,"mammals/pco_mammals.RData"))
   
 #----
   
@@ -50,22 +50,22 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
   
   ###Chosing between birds and mammals
     
-    # pco_data <- pco_birds
-    # traits <- birdstrait
-    # FR_data <- FR_birds
-    # taxa <- "birds"
+    pco_data <- pco_birds
+    traits <- birdstrait
+    FR_data <- FR_birds
+    taxa <- "birds"
     
-    pco_data <- pco_mammals
-    traits <- mammalstrait
-    FR_data <- FR_mammals
-    taxa <- "mammals"
+    # pco_data <- pco_mammals
+    # traits <- mammalstrait
+    # FR_data <- FR_mammals
+    # taxa <- "mammals"
   
   ###histo of first eigenvalues 
   
     df <- data.frame(Eigenvalues = paste0('E',seq(1:8)),
                      Value = pco_data$values$Relative_eig[1:8])
     
-    ggplot(data=df, aes(x=Eigenvalues, y=Value)) + geom_bar(stat="identity",fill="steelblue")+
+    hist_eigen <- ggplot(data=df, aes(x=Eigenvalues, y=Value)) + geom_bar(stat="identity",fill="steelblue")+
       geom_text(aes(label=round(Value,digits=2)), vjust=1.6, color="white", size=3.5)+
       theme_minimal()
   
@@ -102,7 +102,8 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
     traits_pcoa <- merge(traits,pco_data$vectors,by="row.names",all.x=TRUE)
     
     cor_test <- do.call(rbind,lapply(colnames(traits),fun <- function(vname){
-      #vname <- colnames(mamalstrait)[1]
+      
+      #vname <- colnames(traits)[1]
       
       if(is.numeric(traits_pcoa[1,vname])){
         
@@ -133,19 +134,19 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
       cbind.data.frame(var=vname,test=test ,ax1=ax1 ,ax2=ax2,ax3=ax3)
       
     }))
-  
+   
+    dev.off()
     grid.table(cor_test)
     
-  ###Corelations between traits and distinctivness & restrictedness
+  ###Corelations between traits and Di or Ri 
     
+    traits_FR <- merge(traits,FR_data$FR,by="row.names",all.x=TRUE)
+    #TODO il faut reprendre la fonction cor_test_FR qui marche pour les mammals mais pas pour les birds 
     cor_test_FR <- do.call(rbind,lapply(colnames(traits),fun <- function(vname,var="Din"){
       
-      
-      ##var="Din"
-      ##vname <- colnames(mamalstrait)[1]
-      
-      traits_FR <- merge(traits,FR_data$FR,by="row.names",all.x=TRUE)
-      
+      var="Din"
+      vname <- colnames(traits)[1]
+
       if(is.numeric(traits_FR[1,vname])){
         
         Din <- cor(scale(traits_FR[,vname],center=TRUE, scale=TRUE),traits_FR$Din, method="spearman")
@@ -175,12 +176,13 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
                    parse = FALSE, padding = unit(c(2, 2), "mm"))
     tbl <- tableGrob(cor_test_FR, rows=NULL, theme = ttheme_minimal(base_size = 04))
     
+    dev.off()
     grid.table(cor_test_FR)
     
-  ##Correlation between pcoa axes vs. distinctivness & restrictedness 
+  ##Correlation between pcoa axes and Di or Ri 
     
     cor_pcoa <- function(data,pco,var){
-      # data=FR_mammals
+      # data=FR_data
       # pco=pco_data
       # var="Din"
       
@@ -199,9 +201,9 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
       grid.arrange(a,b,c,d,ncol=2)
     }
     
-    cor_pcoa(data=data,pco=pco_data,var="Din")
+    cor_pcoa(data=FR_data,pco=pco_data,var="Din")
     
-    cor_pcoa(data=data,pco=pco_data,var="Rin")
+    cor_pcoa(data=FR_data,pco=pco_data,var="Rin")
   
   
   ###Exemple of species 
@@ -212,45 +214,44 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
     
     #sub <- subset(traits_pcoa,((traits_pcoa$Axis.3 < -0.25) & ((traits_pcoa$Axis.2 > -0.02) & (traits_pcoa$Axis.2 < 0.02))))
     
-    idtotaxo(id=sample(sub$Row.names,1), who="mammals") # TODO need to change the path in the data (pàb with 50km)
+    idtotaxo(id=sample(sub$Row.names,1), who="mammals") # TODO la fonction idtotaxo ne marche plus (attention aussi au 50km pour les birds)
     
     ###subspecies 
     
     subspecies<-function(data,pco,axis.x,axis.y,var1,var2,Q1,Q2,DR){
       
-      # data=FR_mamals_all
-      # pco=pco_mamals
-      # axis.x=1
-      # axis.y=2
-      # var1="Din"
-      # var2="Rin"
-      # Q1="Q75_D"
-      # Q2="Q75_R"
-      # DR="DR75"
+      data=FR_data
+      pco=pco_data
+      axis.x=1
+      axis.y=2
+      var1="Din"
+      var2="Rin"
+      Q1="Q75_D"
+      Q2="Q75_R"
+      DR="DR75"
       
       df <- data.frame(x = pco$vectors[,axis.x],
                        y = pco$vectors[,axis.y],
                        z1 = data$FR[rownames(pco$vectors),var1],
-                       z2 = data$FR[rownames(pco$vectors),var2],
-                       frd= data$FR[rownames(pco$vectors),"FRD_A"])
+                       z2 = data$FR[rownames(pco$vectors),var2])
       
       if (DR=="DR75") df <- data.frame(df,w=df$z1>as.numeric(data$Q[Q1][[1]]) & df$z2>as.numeric(data$Q[Q2][[1]]))
       if (DR=="DR25") df <- data.frame(df,w=df$z1<as.numeric(data$Q[Q1][[1]]) & df$z2<as.numeric(data$Q[Q2][[1]]))
       
-      names(df)[6]<-"w"
+      names(df)[5]<-"w"
       
       df[df$w==TRUE,] 
       
     }
     
-    df_sub <- subspecies(data=FR_mamals_all, pco=pco_mamals, axis.x=2, axis.y=3,
+    df_sub <- subspecies(data=FR_data, pco=pco_data, axis.x=2, axis.y=3,
                          var1="Din", var2="Rin", Q1="Q25_D", Q2="Q25_R", DR="DR25")
     
     sub <- subset(df_sub,(df_sub$x < 0 & df_sub$y < -0.15))
     
     sub <- subset(df_sub,(((df_sub$x < -0.1) & (df_sub$x > -0.2)) & ((df_sub$y < -0.1) & (df_sub$y > -0.14))))
     
-    idtotaxo(id=sample(rownames(sub),1), who="mammals")
+    idtotaxo(id=sample(rownames(sub),1), who="mammals") # TODO la fonction idtotaxo ne marche plus 
     
     head(df_sub)
 #----
@@ -259,19 +260,20 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
     
   ##Chosing between birds and mammals
     
-    # pco_data <- pco_birds
-    # traits <- birdstrait
-    # FR_data <- FR_birds
-    # taxa <- "birds"
+    pco_data <- pco_birds
+    traits <- birdstrait
+    FR_data <- FR_birds
+    taxa <- "birds"
     
-    pco_data <- pco_mammals
-    traits <- mammalstrait
-    FR_data <- FR_mammals
-    taxa <- "mammals"
+    # pco_data <- pco_mammals
+    # traits <- mammalstrait
+    # FR_data <- FR_mammals
+    # taxa <- "mammals"
     
 
     ##Plot pcoa for Din & Rin; the function can only be called for var with quantiles  
     
+    #TODO : cela ne marche pas avec les oiseaux ?? pb avec la le hull " finite coordinates are needed" 
     pcoa.funk.onevar<-function(data,pco,var,plotpdf,resultdir,axis.x,axis.y,jitval,pts,hull,Q1){
       
       # data=data
@@ -319,38 +321,38 @@ load(file=file.path(results_dir,"mammals/50km/FR_mammals.RData"))
     }
     
     a <- pcoa.funk.onevar(data=FR_data,pco=pco_data,var="Din",
-                          plotpdf=FALSE,resultdir=taxa,axis.x=1,axis.y=2,
+                          plotpdf=FALSE,resultdir=taxa,axis.x=2,axis.y=4,
                           jitval=500,pts=TRUE,hull=TRUE,Q1="Q90_D")
     
     b <- pcoa.funk.onevar(data=FR_data,pco=pco_data,var="Rin",
-                          plotpdf=FALSE,resultdir=taxa,axis.x=1,axis.y=2,
+                          plotpdf=FALSE,resultdir=taxa,axis.x=2,axis.y=4,
                           jitval=500,pts=TRUE,hull=TRUE,Q1="Q90_R")
     
     grid.arrange(a,b,ncol=2)
     
     #Plot pcoa with visualisation of D and hull of DR =  D75R75, D25R25, D25R75, D75R25, D75 or R75
     
+    #TODO : cela ne marche pas avec les oiseaux ?? pb avec la le hull " finite coordinates are needed" 
     pcoa.funk.dr<-function(data,pco,plotpdf,resultdir,axis.x,axis.y,jitval,var1,var2,Q1,Q2,DR,Funk){
       
-      # data=FR_mamals_all
-      # pco=pco_mamals
-      # resultdir="mamals"
-      # plotpdf=FALSE
-      # axis.x=2
-      # axis.y=3
-      # jitval=500
-      # var1="Din"
-      # var2="Rin"
-      # Q1="Q75_D"
-      # Q2="Q75_R"
-      # DR="D75R75"
-      # Funk="Din"
+      data=FR_mammals
+      pco=pco_mammals
+      resultdir="mammals"
+      plotpdf=FALSE
+      axis.x=2
+      axis.y=3
+      jitval=500
+      var1="Din"
+      var2="Rin"
+      Q1="Q75_D"
+      Q2="Q75_R"
+      DR="D75R75"
+      Funk="Din"
       
       df <- data.frame(x = jitter(pco$vectors[,axis.x],jitval),
                        y = jitter(pco$vectors[,axis.y],jitval),
                        z1 = data$FR[rownames(pco$vectors),var1],
-                       z2 = data$FR[rownames(pco$vectors),var2],
-                       frd= data$FR[rownames(pco$vectors),Funk])
+                       z2 = data$FR[rownames(pco$vectors),var2])
       
       if (DR=="D75R75") df <- data.frame(df,w=df$z1>as.numeric(data$Q[Q1][[1]]) & df$z2>as.numeric(data$Q[Q2][[1]]))
       if (DR=="D25R25") df <- data.frame(df,w=df$z1<as.numeric(data$Q[Q1][[1]]) & df$z2<as.numeric(data$Q[Q2][[1]]))
