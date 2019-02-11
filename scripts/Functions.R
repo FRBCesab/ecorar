7318439b959cb47afb1f7268105a0c84334a29af
LoiseauN <nicolas.loiseau@cnrs.fr>
2019-02-11 19:21:46 +0100

    # Add several function to draw phylogeny # Plot Phylo # TODO: Compute D

--- scripts/Functions.R
+++ scripts/Functions.R
@@ -1,4 +1,193 @@
 #FUNCTIONS 
+#----
+# COLOR TERMINAL BRANCHES: function to color only the last branch from Nick Crouch's GITHUB
+# phy: object of class phylo
+# data: named numeric vector
+# breaks: number of color breaks
+# cols: character vector of length 2, start col, end col
+# non.terminal.col: vector character length 1, color non terminal branches
+
+color.terminal.branches <- function(phy, data, breaks=6, cols=c("black", "red"), non.terminal.col= "black", edge.width=1, col.bias=1, legend.title = "", show.tip.label=FALSE, alt.col.data=NULL,...){
+  
+  sapply(phy$tip.label, check.spp.search.duplicates, phy$tip.label)
+  
+  # Number of tips in the phylogeny
+  n.tips <- length(phy$tip.label)
+  
+  # This will be the main vector which colors will be assigned to
+  col.vector <- vector(mode="character",length=nrow(phy$edge))
+  
+  # Non-terminal branches will be black
+  col.vector[phy$edge[,2]>n.tips] <- non.terminal.col
+  
+  
+  # Create a vector of colors 
+  colors <-  colorRampPalette(cols, bias=col.bias)(breaks)
+  
+  # range of trait values present
+  if(is.null(alt.col.data)==TRUE){
+    range.vals <- range(data)
+  } else {
+    range.vals <- range(c(alt.col.data, data))
+  }
+  diff <- range.vals[2] - range.vals[1]
+  
+  bin.size <- diff / breaks
+  
+  bin.ref <- as.data.frame(matrix(NA, ncol=3, nrow=breaks))
+  colnames(bin.ref) <- c("Bin","Start","End")
+  bin.ref$Bin <- seq(1, breaks, 1)
+  bin.ref[1,2] <- range.vals[1]
+  bin.ref[1,3] <- range.vals[1] + bin.size
+  
+  for(i in 2:nrow(bin.ref)){
+    
+    bin.ref[i,2] <- bin.ref[i-1,3]+0.00001
+    bin.ref[i,3] <- bin.ref[i,2] + bin.size
+    
+  }
+  
+  
+  ## for legend
+  legend.text <- vector()
+  
+  for(i in 1:nrow(bin.ref)){
+    
+    t <- paste(as.character(bin.ref[i,2]), " - ",as.character(bin.ref[i,3]))
+    
+    legend.text[i] <- t
+    
+  }
+  
+  
+  # create a duplicate of phy$edge which can be manipulated as required
+  edge.data <- as.data.frame(phy$edge)
+  
+  
+  # Yes, its a loop, but its easier
+  for(i in 1:length(phy$tip.label)){
+    
+    spp <- phy$tip.label[i]
+    
+    spp.trait.val <- as.numeric(data[grep(spp,names(data))])
+    
+    bin.num <- bin.ref[spp.trait.val >= bin.ref$Start & spp.trait.val <= bin.ref$End,1]
+    
+    spp.col <- colors[bin.num]
+    
+    edge.row <- as.numeric(rownames(edge.data[edge.data$V2==i,]))
+    
+    col.vector[edge.row] <- spp.col
+    
+  }
+  
+  max.val <- range.vals[2]
+  
+  #layout(matrix(1:2,ncol=2), width = c(2,1),height = c(1,1))
+  #l <-  matrix(c(1,1,2),ncol=3,nrow=1)
+  #layout(l)
+  #par(mfrow=c(1,2))
+  #par(mai=rep(0.1,4))
+  plot(phy, show.tip.label=show.tip.label, edge.col=col.vector, edge.width=edge.width,type="fan",tip.color="white")
+  
+  #image <- matrix(colors, ncol=1)
+  #image <- as.matrix(rev(legend_image))
+  #legend_image <- as.raster(image)
+  # legend_image <- as.raster(matrix(rev(colors), ncol=1))
+  
+  # plot(c(0,.9),c(0,max.val),type = 'n', axes = F,xlab = '', ylab = '', main = legend.title, xlim=c(0,.2))
+  
+  # rasterImage(legend_image, 0, 0, .08,max.val)
+  # text(x=0.11, y = seq(0,max.val,l=5), labels = seq(0,max.val,l=5))
+}
+
+###
+
+check.spp.search.duplicates <- function(search.spp, all.spp){
+  
+  returns <- grep(search.spp, all.spp)
+  
+  if(length(returns)>1){
+    
+    stop("The species  ",search.spp,"  will cause problems - searching for this species in all the species present will return multiple species, consider renaming!")
+    
+  }
+  
+}
+
+#----
+
+# COLOR TERMINAL BRANCHES: function to compute only the last branch from Nick Crouch's GITHUB
+# phy:        Phylogeny of class 'phylo'
+# data:       'data.frame' containing data, must have a 'Species' column
+# plot.data:  column in data to be plotted. Must be numeric, starting at 1
+# pch:        plotting symbol
+# cols:       vector of cols?
+
+add.tip.icons <- function(phy, data, plot.data="Location", grouping="Species", pch=21, cols=c("red","blue","green"), cex=2){
+  
+  require(ape)
+  
+  if( (plot.data %in% colnames(data)) == FALSE){
+    stop("Specified data to plot does not appear in the column names of the data")
+  }
+  
+  v <- data[,grep(grouping, colnames(data))] %in% phy$tip.label
+  
+  if((FALSE %in% v) == TRUE){
+    stop("Tip labels of phylogeny do not match grouping vector specified")
+  }
+  
+  
+  d <- data[,grep(plot.data, colnames(data))]
+  
+  # does number of unique entries in plot.data equal number of colors provided?
+  num.unique.vals <- length(unique(d))
+  
+  if(num.unique.vals != length(cols)){
+    
+    stop("Number of states does not equal number of colors provided")
+    
+  }
+  
+  names(d) <- data[,grep(grouping, colnames(data))]
+  
+  names <- phy$tip.label
+  
+  m <- match(names, names(d))
+  
+  r <- d[c(m)]
+  
+  plot.col <- vector(length=length(phy$tip.label))
+  
+  for(i in 1:length(cols)){
+    
+    plot.col[as.numeric(r)==i] <- cols[i]
+    
+  }
+  
+  
+  # Plot
+  
+  plot(phy, show.tip.label=FALSE)
+  
+  if(pch < 21){
+    
+    tiplabels(col=plot.col, pch=pch, cex=cex)
+    
+  } else if(pch >= 21 & pch <= 25){
+    
+    tiplabels(bg=plot.col, pch=pch, cex=cex)
+    
+  } else {
+    
+    stop("Invalid plotting symbol value provided")
+    
+  }
+  
+  
+}
+#----
 
 #ARC.CLADELABEL : add arc around phylogenie tree
 #Modifer version of ARC.CLADELABEL from phytools packages that do not allow change in color of arc 
--- scripts/Phylogeny.R
+++ scripts/Phylogeny.R
@@ -149,51 +149,117 @@ library("RColorBrewer")
           rasterImage(lagomorpha,-250,70,-200,110,fg="grey")
  
           
-# Second version of the graph       
+# Second version of the graph      
+          rownames(data_DR)<-as.character(gsub(" ", "_", data_DR$Name))
+          #order in the same order of the phylo
+          data_DR<-data_DR[set_mammals$tip.label,] 
+
+          data_DR$cols <- NA
+          data_DR$cols[data_DR$DR_class=="D75R75"] <- "red"
+          data_DR<-data_DR[rownames(data_DR) %in% mammalsPhy$tip.label,]
+          
           
           # plotting PHYLOGENY TREE
-          plot(set_mammals,type = "fan",edge.color = "grey", show.tip.label = TRUE,tip.color="white", edge.width = 0.4) # ,edge.lty= data_DR$ltystyle,edge.color = data_DR$cols)
-          data_DR$colpointD75R75 <- data_DR$cols
-          data_DR$colpointD75R75[ data_DR$colpointD75R75!="#7AD151FF"]<- NA
-          
-          data_DR$colpointD25R75 <- data_DR$cols
-          data_DR$colpointD25R75[ data_DR$colpointD25R75!="#2A788EFF"]<- NA
-          
-          data_DR$colpointD25R25 <- data_DR$cols
-          data_DR$colpointD25R25[ data_DR$colpointD25R25!="#414487FF"]<- NA
-          
-          tiplabels(pch = 19, col = data_DR$colpointD75R75, cex = 0.4 ,offset=5)
-          tiplabels(pch = 19, col = data_DR$colpointD25R75, cex = 0.4 ,offset=10)
-          tiplabels(pch = 19, col = data_DR$colpointD25R25, cex = 0.4 ,offset=15)
-          
-          # plotting family labels/arcs
-          #   offset <- sample(seq(1.12,1.8, length.out = length(nodesArc)), length(nodesArc), replace = FALSE, prob = NULL)
-            offset <- c(1.552727,1.645455,1.429091,1.181818,1.243636,
-            1.738182,1.614545,1.305455,1.150909,1.398182,
-            1.367273,1.707273,1.769091,1.583636,1.460000,
-            1.212727,1.274545,1.120000,1.505,1.645455,
-            1.336364,1.74,1.435)
-          
+          # plot(set_mammals,type = "fan",edge.color = "grey", show.tip.label = TRUE,tip.color="white", edge.width = 0.4)  # ,edge.lty= data_DR$ltystyle,edge.color = data_DR$cols)
+          color.terminal.branches(set_mammals, rarety, breaks=4, cols=c("gray33","red"), edge.width=c(0.4), show.tip.label=TRUE, non.terminal.col= "gray33")
+          tiplabels(pch = 19, col = data_DR$cols, cex = 0.4 ,offset=5)
 
+          # plotting family labels/arcs
+          offset <- rep(c(1.15,1.20,1.25),length(nodesArc)/2)
+        
+           # rep(c(1.08,1.16,1.24), length(nodesArc)/2)
           for(i in 1:length(nodesArc)){
             
-            if(i %in% c(seq(1,length(nodesArc), by = 2))) laboffset <- 0.02
-            if(i %in% c(seq(2,length(nodesArc), by = 2))) laboffset <- 0.02
-       
-             arc.cladelabels(text= paste0(names(nodesArc)[i]),
+            if(i %in% c(seq(1,length(nodesArc), by = 2))) laboffset <- 0.06
+            if(i %in% c(seq(2,length(nodesArc), by = 2))) laboffset <- 0.06
+            
+            if((i %% 2) == 0){  #If odd 
+              arc.cladelabels(text= paste0(i,""), #paste0(names(nodesArc)[i])
                               node=nodesArc[i],
                               ln.offset=offset[i],
                               lab.offset=offset[i]+laboffset, 
-                              cex = 0.4, 
-                              colarc = "black",
+                              cex = 0.3, 
+                              colarc = "gray55",
                               lwd = 1, 
                               lty = 1, 
                               orientation = "curved",
-                              mark.node = FALSE)  
-             }
+                              mark.node = FALSE,col="gray55")
+              
+            }else{ 
+              arc.cladelabels(text= paste0(i,"") ,#paste0(names(nodesArc)[i])
+                              node=nodesArc[i],
+                              ln.offset=offset2[i],
+                              lab.offset=offset2[i]+laboffset, 
+                              cex = 0.3, 
+                              colarc = "grey",
+                              lwd = 1, 
+                              lty = 1, 
+                              orientation = "curved",
+                              mark.node = FALSE,col="grey")
+            }
+          }
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          for(i in 1:length(nodesArc)){
+            
+              if(i %in% c(seq(1,length(nodesArc), by = 2))) laboffset <- 0.02
+              if(i %in% c(seq(2,length(nodesArc), by = 2))) laboffset <- 0.02
+            
+               arc.cladelabels(text= "  ",# paste0(names(nodesArc)[i]),
+                               node=nodesArc[i],
+                               ln.offset=offset[i],
+                               lab.offset=offset[i]+laboffset, 
+                               cex = 0.5, 
+                               colarc = colours,
+                               lwd = 1, 
+                               lty = 1, 
+                               orientation = "curved",
+                               mark.node = FALSE)
+                          }
+          
+          
+          
+          
+          
+          
+          
+          
+          # offset <- c(1.552727,1.645455,1.429091,1.181818,1.243636,1.738182,1.614545,1.305455,1.150909,1.398182,1.367273,1.707273,1.769091,1.583636,1.460000,
+          #1.212727,1.274545,1.120000,1.505,1.645455, 1.336364,1.74,1.435)
+          
+          #offset <-  sample(seq(1.12,1.8, length.out = length(nodesArc)), length(nodesArc), replace = FALSE, prob = NULL)
+          
+          #for(i in 1:length(nodesArc)){
+            
+          #  if(i %in% c(seq(1,length(nodesArc), by = 2))) laboffset <- 0.02
+          #  if(i %in% c(seq(2,length(nodesArc), by = 2))) laboffset <- 0.02
+       
+          #   arc.cladelabels(text= paste0(names(nodesArc)[i]),
+          #                   node=nodesArc[i],
+          #                   ln.offset=offset[i],
+          #                   lab.offset=offset[i]+laboffset, 
+          #                   cex = 0.4, 
+          #                   colarc = "black",
+          #                   lwd = 1, 
+          #                   lty = 1, 
+          #                   orientation = "curved",
+          #                   mark.node = FALSE)  
+          #  }
           
           
 #Compute Lambda to know if functional rare species are packaged           
+         
+              # LAMBDA IS NOT GOOD FOR BINARY INFO
             
            #Number of species per order 
            sp_order<- unlist(lapply(unique(data_DR$order), function(x){ 
@@ -201,14 +267,22 @@ library("RColorBrewer")
                       names(l) <- x
                       l} ))
             
-            rarety <- data_DR$DR_class
+            rarety <- data_DR$colpointD75R75
             rarety <- as.numeric(as.factor(rarety))
+            rarety[is.na(rarety)]<-0
             names(rarety)<-rownames(data_DR)
             
             lambda<-phytools::phylosig(set_mammals, rarety, method="lambda", test=TRUE)   
           
-          
-          
+#Compute FRITZ to know if functional rare species are packaged                 
+            onction phylo.d() dans le package caper
+            
+            Faut utiliser au préalable comparative.data() pour formater les data 
+            (traits, phylo etc....)
+            
+            Si besoin j'ai du code
+            
+            Fab      
           
 
      
