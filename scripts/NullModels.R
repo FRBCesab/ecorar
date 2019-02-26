800525d8c7edba8e7a4fcef11b2f659dea3c2a44
Nicolas Loiseau <nicolasloiseau@wificampus-031116.grenet.fr>
2019-02-26 15:44:41 +0100

    # Compute null models need server!!

--- scripts/NullModels.R
+++ scripts/NullModels.R
@@ -3,8 +3,6 @@ rm(list=ls(all=TRUE))
 source("./scripts/Functions.R")
 who.remote(remote=FALSE,who="NL")
 
-FR_mammals
-
 data_DR_mammals<-FR_mammals$FR
 QD75 <- FR_mammals$Q$Q75_D
 QD25 <- FR_mammals$Q$Q25_D
@@ -20,16 +18,15 @@ data_DR_mammals$DR_class[(((data_DR_mammals$Din>QD25) & (data_DR_mammals$Din<QD7
 data_DR_mammals<-data.frame(data_DR_mammals[,"DR_class"],row.names = rownames(data_DR_mammals))
 colnames(data_DR_mammals) <- "DR_class"
 
-
-##Generate main results
-SES.final.results <- function(ids,proc,occ_mat_list,sub_data){
+##Generate number of species per DR_class
+Nb.DR_class<- function(ids,proc,occ_mat_list,data_DR_null){
   
   # proc <- 50
   # occ_mat_list <- occ_mammals_list
   # ids <- names(occ_mat_list)
-  # sub_data <- sub_mammals
+  # data_DR_null <- data_DR_mammals
   
-  SES_funk_all <-do.call(rbind,mclapply(ids,function(id) {    #cat("id:",i,"\n")
+  Null_funk_all <-do.call(rbind,mclapply(ids,function(id) {    #cat("id:",i,"\n")
     
     #id <- ids[10000]
     
@@ -37,11 +34,11 @@ SES.final.results <- function(ids,proc,occ_mat_list,sub_data){
     spe_sub <- unique(occ_mat_list[[id]])
     
     #Number of species within a sites with FR values above quantiles of species and functional distribution 
-    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D75R75"))])>0) D75R75=length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D75R75"))]) else D75R75=0
-    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D25R75"))])>0) D25R75=length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D25R75"))]) else D25R75=0
-    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D75R25"))])>0) D75R25=length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D75R25"))]) else D75R25=0
-    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D25R25"))])>0) D25R25=length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="D25R25"))]) else D25R25=0
-    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="AVG"))])>0) AVG=length(spe_sub[spe_sub %in% rownames(subset(data_DR_mammals,data_DR_mammals$DR_class=="AVG"))]) else AVG=0
+    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D75R75"))])>0) D75R75=length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D75R75"))]) else D75R75=0
+    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D25R75"))])>0) D25R75=length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D25R75"))]) else D25R75=0
+    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D75R25"))])>0) D75R25=length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D75R25"))]) else D75R25=0
+    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D25R25"))])>0) D25R25=length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="D25R25"))]) else D25R25=0
+    if (length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="AVG"))])>0) AVG=length(spe_sub[spe_sub %in% rownames(subset(data_DR_null,data_DR_null$DR_class=="AVG"))]) else AVG=0
     
     #combine all 
     res <- cbind.data.frame(id,AVG,D75R75,D25R25,D75R25,D25R75)
@@ -51,13 +48,23 @@ SES.final.results <- function(ids,proc,occ_mat_list,sub_data){
     
   },mc.cores = proc))
   
-  return(funk_all)
+  return(Null_funk_all)
 }
 
-data_DR_mammals
+data_DR_randomize_mammals <- lapply(1:10, function(i) data.frame(sample(data_DR_mammals$DR_class),row.names = rownames(data_DR_mammals)))
+data_DR_randomize_mammals <- lapply(data_DR_randomize_mammals, setNames, "DR_class")
+
+SES_funk <- lapply(1:10,function(i) {Nb.DR_class(ids= names(occ_mat_list)[1000:1010],data_DR_null=data_DR_randomize_mammals[[i]],occ_mammals_list,proc=2)})  
+# Change the first column to rownames and drop this column 
+SES_funk <- lapply(SES_funk, function(df) {df_out <- df[,-1]
+rownames(df_out) <- df[[1]]
+df_out
+})
+Null_mean <-  as.data.frame(plyr::aaply(laply(SES_funk, as.matrix), c(2, 3), mean))
+Null_sd <- as.data.frame(plyr::aaply(laply(SES_funk, as.matrix), c(2, 3), sd))
+
+SES_funk_mammals <- funk_mammals[,c(3:7)]
+SES_funk_mammals <- (SES_funk_mammals - Null_mean)/Null_sd
+  
 
 
-SES_funk_birds <-
-SES_funk_mammals <- 
-
-(obs - mean(null))/sd(null) )
