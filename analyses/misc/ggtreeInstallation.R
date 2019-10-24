a74c4d0f9243a2048dd1858e73340ed84fa73559
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-10-24 18:18:57 +0200

    Rename Scripts

--- analyses/misc/ggtreeInstallation.R
+++
@@ -1,9 +0,0 @@
-#'  -------------------------------------------------------------------------   @Installation2ggtree
-
-
-if (!requireNamespace("BiocManager", quietly = TRUE)) {
-
-  install.packages("BiocManager")
-}
-
-BiocManager::install("ggtree")
